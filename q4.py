70cb31dece46f5f9039eb40b40dc9de573e6d172
abhiram <abhiramashok1062004@gmail.com>
2025-08-09 16:56:18 +0530

    Question 4

--- q4.py
+++ q4.py
@@ -22,28 +22,26 @@ class Solution(object):
 
 # Another solution
 
-class Solution(object):
-    def romanToInt(self, s):
-        symbols = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
-
-        number = 0
-        i = 0
-        while i < len(s):
-            if i + 1 < len(s) and symbols[s[i]] < symbols[s[i + 1]]:
-                number += symbols[s[i + 1]] - symbols[s[i]]
-                i += 2
-            else:
-                number += symbols[s[i]]
-                i += 1
-
-        return number
-
-
+# class Solution(object):
+#     def romanToInt(self, s):
+#         symbols = {
+#             'I': 1,
+#             'V': 5,
+#             'X': 10,
+#             'L': 50,
+#             'C': 100,
+#             'D': 500,
+#             'M': 1000
+#         }
+#
+#         number = 0
+#         i = 0
+#         while i < len(s):
+#             if i + 1 < len(s) and symbols[s[i]] < symbols[s[i + 1]]:
+#                 number += symbols[s[i + 1]] - symbols[s[i]]
+#                 i += 2
+#             else:
+#                 number += symbols[s[i]]
+#                 i += 1
+#
+#         return number
