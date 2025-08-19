d624d5debb3050fe0f1ff62280e4d79b45ffe64d
abhiram <abhiramashok1062004@gmail.com>
2025-08-19 21:42:43 +0530

    Question 47

--- q47.py
+++ q47.py
@@ -1,14 +1,15 @@
 class Solution(object):
     def reverseVowels(self, s):
         vowels = 'aeiouAEIOU'
-        vowelIndices = []
-        for i, char in enumerate(s):
-            if char in vowels:
-                vowelIndices.append(i)
-        outStr = ""
-        for i in range(len(s)):
-            if i in vowelIndices:
-                outStr += s[vowelIndices[len(vowelIndices) - vowelIndices.index(i)]]
-            else:
-                outStr += s[i]
-        return outStr
+        s = list(s)
+        i, j = 0, len(s)-1
+        while i < j:
+            if s[i] in vowels and s[j] in vowels:
+                s[i], s[j] = s[j], s[i]
+                i += 1
+                j -= 1
+            if s[i] not in vowels:
+                i += 1
+            if s[j] not in vowels:
+                j -= 1
+        return ''.join(s)
